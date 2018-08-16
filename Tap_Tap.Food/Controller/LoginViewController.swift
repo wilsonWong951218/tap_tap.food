0fad78825cf0d37340fe3a992a2e43a36c8c9af5
Tsejunzhen <victorjunzhen@gmail.com>
2018-08-16 11:13:40 +0700

    storyboard merge

---
+++ Tap_Tap.Food/Controller/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Tap_Tap.Food
+//
+//  Created by larvataAndroid on 2018/8/16.
+//  Copyright © 2018年 LAT. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func LoginSuccessButtonAction(_ sender: UIButton) {
+        let startVC = ViewController()
+        navigationController?.pushViewController(startVC, animated: true)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
