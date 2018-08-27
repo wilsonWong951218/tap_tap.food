b4f73008a4d51cbdfc6cad0bb69c90647ef4a087
Tsejunzhen <victorjunzhen@gmail.com>
2018-08-27 10:36:26 +0700

    signup only

--- Tap_Tap.Food/Controller/LoginViewController.swift
+++ Tap_Tap.Food/Controller/LoginViewController.swift
@@ -7,32 +7,86 @@
 //
 
 import UIKit
+import FirebaseAuth
+
+let primaryColor = UIColor(red: 210/255, green: 109/255, blue: 180/255, alpha: 1)
+let secondaryColor = UIColor(red: 52/255, green: 148/255, blue: 230/255, alpha: 1)
 
 class LoginViewController: UIViewController {
 
-    @IBAction func LoginSuccessButtonAction(_ sender: UIButton) {
-        let startVC = MainVC()
-        navigationController?.pushViewController(startVC, animated: true)
+    var continueButton:RoundedWhiteButton!
+    var activityView:UIActivityIndicatorView!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var userNameField: UITextField!
+    
+    @IBOutlet weak var emailField: UITextField!
+//    @IBAction func LoginSuccessButtonAction(_ sender: UIButton) {
+//        let startVC = MainVC()
+//        navigationController?.pushViewController(startVC, animated: true)
+//    }
+    
+    @IBAction func continueButton(_ sender: UIButton) {
+        authSignUp()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initFrame()
+    }
+    func authSignUp() {
+        guard let userName = userNameField.text else {
+            return
+        }
+        guard let email = emailField.text else {
+            return
+        }
+        guard let password = passwordField.text else {
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: password) { user, error in
+            if error == nil && user != nil {
+                print("Sucess")
+            } else {
+                print("Error: \(error!.localizedDescription)")
+            }
+        }
     }
 
+    func initFrame() {
+        backGroundDesign()
+    }
+    func backGroundDesign() {
+        let gradient = CAGradientLayer()
+        gradient.frame = self.view.frame
+        gradient.colors = [
+            UIColor(red: 1, green: 0.8157, blue: 0, alpha: 1).cgColor,
+            UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+        ]
+        gradient.startPoint = CGPoint(x:0, y:0)
+        gradient.endPoint = CGPoint(x:1, y:1)
+        self.view.layer.insertSublayer(gradient, at: 0)
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "colors")
+        gradientChangeAnimation.duration = 10.0
+        gradientChangeAnimation.toValue = [
+            UIColor(red: 0.9882, green: 0.5412, blue: 0, alpha: 1.0).cgColor,
+            UIColor(red: 1, green: 0.8157, blue: 0, alpha: 1).cgColor
+        ]
+        gradientChangeAnimation.fillMode = kCAFillModeForwards
+        gradientChangeAnimation.isRemovedOnCompletion = false
+        gradient.add(gradientChangeAnimation, forKey: "colorChange")
+    }
+
+    func setContinueButton(enabled:Bool) {
+        if enabled {
+            continueButton.alpha = 1.0
+            continueButton.isEnabled = true
+        } else {
+            continueButton.alpha = 0.5
+            continueButton.isEnabled = false
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Tap_Tap.Food/Controller/RoundedWhiteButton.swift
@@ -0,0 +1,66 @@
+//
+//  RoundedWhiteButton.swift
+//  Tap_Tap.Food
+//
+//  Created by larvataAndroid on 2018/8/27.
+//  Copyright © 2018年 LAT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class RoundedWhiteButton:UIButton {
+    
+    var highlightedColor = UIColor.white
+    {
+        didSet {
+            if isHighlighted {
+                backgroundColor = highlightedColor
+            }
+        }
+    }
+    var defaultColor = UIColor.clear
+    {
+        didSet {
+            if !isHighlighted {
+                backgroundColor = defaultColor
+            }
+        }
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                backgroundColor = highlightedColor
+                
+            } else {
+                backgroundColor = defaultColor
+            }
+        }
+    }
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
+    }
+    
+    func setup() {
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = 2.0
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
+}
+
