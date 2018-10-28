c2d4d324afae821f6b8bf7c15b4b648fd719352f
Tsejunzhen <resy77799@gmail.com>
2018-10-28 16:10:40 +0800

    Added:Animation , firstPage/mainVC Fixed:closeButton set notify animate

--- Tap_Tap.Food/Controller/MainVC.swift
+++ Tap_Tap.Food/Controller/MainVC.swift
@@ -15,7 +15,9 @@ class MainVC: UIViewController,CLLocationManagerDelegate {
     
     @IBOutlet weak var animationView: UIView!
     var animationArray = [LOTAnimationView]()
-    let animationViewTapped = LOTAnimationView(name: "tapped")
+    let animationViewTapped = LOTAnimationView(name: "circle")
+//    let animationViewTapped_2 = LOTAnimationView(name: "home")
+
     var animationViewLoop = LOTAnimationView()
     var timer:Timer? = nil
     private let locationManager = CLLocationManager()
@@ -28,64 +30,40 @@ class MainVC: UIViewController,CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let notify = Notification.Name("initAnimate")
+        
+        NotificationCenter.default.addObserver(self , selector: #selector(setUpAnimation), name: notify, object: nil)
+
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
+        
         setUpSideMenu()
         setUpAnimation()
+        
         buttonView.addTarget(self, action: #selector(tappedButton), for: .allTouchEvents)
+        
         locationNow()
-        NotificationCenter.default.addObserver(self, selector: #selector(locationChange), name: NSNotification.Name(rawValue: "LocationChange"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(locationChange), name: NSNotification.Name(rawValue: "UpdateLocation"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(setRadius(_:)), name: NSNotification.Name(rawValue: "UpdateRadius"), object: nil)
-
-    }
+        
+        notificationAddObserver()
+        }
     
-    func randomAnimation() {
-        animationViewLoop = LOTAnimationView(name: String(arc4random_uniform(2)))
-    }
-    
-    @objc fileprivate func tappedButton(_ sender: UIButton) {
-        print(sender.tag)
-        buttonView.isHidden = true
-        locationNow()
-        animationViewTapped.frame = CGRect(x: 0, y: 0, width: self.animationView.frame.size.width, height: self.animationView.frame.size.height)
-        animationViewTapped.contentMode = .scaleAspectFill
-        animationViewTapped.loopAnimation = true
-        self.animationView.addSubview(animationViewTapped)
-        timer = Timer.scheduledTimer(timeInterval: 5,target:self,selector:#selector(handleTimer),userInfo:nil,repeats:false)
-        animationViewTapped.play()
-    }
-    
-    @objc fileprivate func handleTimer() {
-        animationViewTapped.stop()
-        buttonView.isHidden = false
-        performSegue(withIdentifier: "toSearchDonePop", sender: nil)
-    }
-    
-    private func setUpAnimation() {
+    func closeButtonReset() {
+        randomAnimation()
+        animationViewLoop.isHidden = false
         animationViewLoop.frame = CGRect(x: 0, y: 0, width: self.animationView.frame.size.width, height: self.animationView.frame.size.height)
         animationViewLoop.contentMode = .scaleAspectFill
         animationViewLoop.loopAnimation = true
         self.animationView.addSubview(animationViewLoop)
-//      animationViewLoop.play()
-        }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        self.navigationController?.navigationBar.barTintColor = UIColor.white
-        self.navigationController?.navigationBar.tintColor = UIColor.orange
-        let textAttributes = [NSAttributedStringKey.foregroundColor:UIColor(red: 253/255, green: 102/255, blue: 78/255, alpha: 1)]
-        self.navigationController?.navigationBar.titleTextAttributes = textAttributes
+        animationViewLoop.play()
     }
-
     
     fileprivate func setUpSideMenu(){
         SideMenuManager.default.menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? UISideMenuNavigationController
         
         
         //三种不同的SideManu的显示.menuSlideIn, .viewSlideInOut, .menuDissolveIn
-         SideMenuManager.default.menuPresentMode = .viewSlideInOut
-
+        SideMenuManager.default.menuPresentMode = .viewSlideInOut
+        
         //背后荧幕缩放1为初始值
         SideMenuManager.default.menuAnimationTransformScaleFactor = CGFloat(1)
         SideMenuManager.default.menuWidth = view.frame.width * CGFloat(0.8)
@@ -94,9 +72,76 @@ class MainVC: UIViewController,CLLocationManagerDelegate {
         SideMenuManager.default.menuAnimationFadeStrength = CGFloat(0)
         SideMenuManager.default.menuShadowOpacity = Float(0)
         SideMenuManager.default.menuAnimationBackgroundColor = UIColor(red: 252/255, green: 138/255, blue: 77/255, alpha: 1)
-       
+        
     }
     
+    @objc private func setUpAnimation() {
+        randomAnimation()
+        
+        animationViewLoop.frame = CGRect(x: 0, y: 0, width: self.animationView.frame.size.width, height: self.animationView.frame.size.height)
+        animationViewLoop.contentMode = .scaleAspectFill
+        animationViewLoop.loopAnimation = true
+        self.animationView.addSubview(animationViewLoop)
+        animationViewLoop.play()
+    }
+    
+    fileprivate func locationNow(){
+        locationManager.startUpdatingLocation()
+        guard let location = locationManager.location else {return}
+        fetchNearbyPlaces(coordinate: location.coordinate)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func notificationAddObserver(){
+    NotificationCenter.default.addObserver(self, selector: #selector(locationChange), name: NSNotification.Name(rawValue: "LocationChange"), object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(locationChange), name: NSNotification.Name(rawValue: "UpdateLocation"), object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(setRadius(_:)), name: NSNotification.Name(rawValue: "UpdateRadius"), object: nil)
+    }
+
+    func randomAnimation() {
+        animationViewLoop = LOTAnimationView(name: "0")// String(arc4random_uniform(2)))
+    }
+    
+    func setUpTappedAnimation() {
+        animationViewLoop.stop()
+        animationViewLoop.isHidden = true
+        animationViewTapped.frame = CGRect(x: 0, y: 0, width: self.animationView.frame.size.width, height: self.animationView.frame.size.height)
+        animationViewTapped.contentMode = .scaleAspectFill
+        animationViewTapped.loopAnimation = true
+        self.animationView.addSubview(animationViewTapped)
+        animationViewTapped.play()
+    }
+    
+    @objc fileprivate func tappedButton(_ sender: UIButton) {
+        print(sender.tag)
+        buttonView.isHidden = true
+        locationNow()
+        setUpTappedAnimation()
+        timer = Timer.scheduledTimer(timeInterval: 5,target:self,selector:#selector(handleTimer),userInfo:nil,repeats:false)
+        animationViewTapped.play()
+    }
+    
+    @objc fileprivate func handleTimer() {
+        animationViewTapped.stop()
+        buttonView.isHidden = false
+        
+        performSegue(withIdentifier: "toSearchDonePop", sender: nil)
+    }
+    @objc func notifyGet(noti: Notification) {
+        
+        
+    }
+        
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+       
+        
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+        self.navigationController?.navigationBar.tintColor = UIColor.orange
+        let textAttributes = [NSAttributedStringKey.foregroundColor:UIColor(red: 253/255, green: 102/255, blue: 78/255, alpha: 1)]
+        self.navigationController?.navigationBar.titleTextAttributes = textAttributes
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toSearchDonePop"{
             let passData = segue.destination as! SearchDonePopVC
@@ -113,6 +158,7 @@ class MainVC: UIViewController,CLLocationManagerDelegate {
         }
     }
 }
+
 
 extension MainVC: UISideMenuNavigationControllerDelegate {
     
@@ -162,12 +208,7 @@ extension MainVC{
     }
 
     
-    fileprivate func locationNow(){
-        locationManager.startUpdatingLocation()
-        guard let location = locationManager.location else {return}
-        fetchNearbyPlaces(coordinate: location.coordinate)
-        locationManager.stopUpdatingLocation()
-    }
+    
     
     @objc fileprivate func locationChange(){
         locationManager.startUpdatingLocation()
